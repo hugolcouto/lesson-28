09e32bf6eb94908db80ad6f12dcd4cafb125844d
Hugo Couto <hugo.couto@webhub.com.br>
2020-02-03 09:52:41 -0300

    180. Redux Thunk Into Saga

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,8 +1,17 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
 import ShopActionTypes from './shop.types';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions';
 
 export function* fetchCollectionAsync() {
-    yield console.log('Hello World');
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+        yield put(fetchCollectionsSuccess(collectionsMap));
+    } catch(error) {
+        yield put(fetchCollectionsFailure(error.message));
+    };
 }
 
 export function* fetchCollectionStart() {
